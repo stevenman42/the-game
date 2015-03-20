2f39bea58d8f38800efaf7b300670d9dd4bfcd18
Steven <stevenman42@gmail.com>
2015-03-20 19:44:56 -0400

    I'm stupid

---
+++ StupidGame.java
@@ -0,0 +1,19 @@
+//test
+
+//steven is this even giong to correctly load our files?
+//i dont think thisll work with java
+import java.util.Scanner;
+
+public class StupidGame {
+	public void printThing(String thing)
+	{
+		System.out.println(thing);
+	}	
+
+  public static void main(String args[]){
+    Scanner kb = new Scanner(System.in);
+    String str1 = kb.nextLine();
+    System.out.println(str1 + "420Blaze"+ "get rekt");
+  }
+  
+}
