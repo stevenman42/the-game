9073ffac621e0060cce28c00f896aa5f5cb4a871
bespers <bespeboy@gmail.com>
2015-03-20 19:35:03 -0400

    Game
    Uploaded my game files as is for fishEvo

---
+++ FishEvolution/src/com/fishEvo/main/Game.java
@@ -0,0 +1,55 @@
+package com.fishEvo.main;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable{
+	
+	private boolean running = false;
+
+
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime) /ns;
+			lastTime = now;
+			while(delta >= 1){
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+			//	System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();		
+	}
+	
+	//start the game (through starting the thread)
+	public synchronized void start(){
+		thread  = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	//if it fails give a reason why
+	public synchronized void stop(){
+		try{
+			thread.join();
+			running = false;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
